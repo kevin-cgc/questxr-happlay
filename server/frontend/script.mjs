b3e8f51d702e6bc48c46c5b9d661a205f2c547e4
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2024-02-25 20:37:11 -0700

    support click to load file

--- server/frontend/script.mjs
+++ server/frontend/script.mjs
@@ -68,52 +68,92 @@ dragndroptacton_form.addEventListener("dragleave", event => {
 	event.preventDefault();
 	dragndroptacton_form.classList.remove("dragover");
 });
+dragndroptacton_form.addEventListener("click", async () => {
+	try {
+		if (!("showOpenFilePicker" in window)) {
+			alert("File picker not supported in this browser");
+			return;
+		}
+		const [file_handle] = await window.showOpenFilePicker({
+			types: [{
+				description: "Haptic PCM (8KHz+F32LE ideally)",
+				accept: {
+					"audio/wav": [".wav", ".WAV", ".wave", ".WAVE"],
+					"audio/aac": [".aac", ".AAC"],
+					"audio/ogg": [".ogg", ".OGG"],
+					"audio/opus": [".opus", ".OPUS"],
+					"audio/webm": [".webm", ".WEBM", ".weba", ".WEBA"],
+					"audio/flac": [".flac", ".FLAC"],
+					"audio/x-flac": [".flac", ".FLAC"],
+				},
+			},],
+			multiple: false,
+			excludeAcceptAllOption: false,
+		});
+		const file = await file_handle.getFile();
+		load_and_send_pcm(file);
+	} catch (e) {
+		if (e.name == "AbortError") {
+			//do nothing
+		} else {
+			throw e;
+		}
+	}
+});
 dragndroptacton_form.addEventListener("drop", event => {
 	event.preventDefault();
 	dragndroptacton_form.classList.remove("dragover");
 	const files = event.dataTransfer.files;
 	if (files.length > 0) {
 		const file = files[0];
-		// if (file.type !== "audio/wav") { // lets just let decodeAudioData complain
-		// 	alert("Invalid file type, must be audio/wav");
-		// 	return;
-		// }
-		if (file.size > 5e6) {
-			alert("File size should be less than 5MB (soft limit, can remove if needed)");
-			return;
-		}
-
-		const reader = new FileReader();
-		reader.onload = async () => {
-			try {
-				const data = reader.result;
-				if (typeof data === "string") throw new TypeError("unreachable");
-
-				const audio_ctx = new AudioContext({ sampleRate: SAMPLE_RATE, latencyHint: "playback" });
-				const decoded_pcm = await audio_ctx.decodeAudioData(data);
-				console.log(decoded_pcm);
-
-				if (decoded_pcm.numberOfChannels !== 1) {
-					alert("Audio file is not mono");
-					return;
-				}
-
-				const pcm = decoded_pcm.getChannelData(0); // device expects f32le
-
-				draw_waveform(pcm);
-
-				swslog(`Sending ${pcm.length} samples (${decoded_pcm.duration}s) to devices...`);
-				ws.send(pcm.buffer);
-			} catch (err) {
-				console.error(err);
-				alert("Error reading file, see browser console for details");
-			}
-		};
-		reader.readAsArrayBuffer(file);
+		load_and_send_pcm(file);
 	} else {
 		alert("No files dropped");
 	}
 });
+
+/**
+ *
+ * @param {File} file
+ */
+function load_and_send_pcm(file) {
+	// if (file.type !== "audio/wav") { // lets just let decodeAudioData complain
+	// 	alert("Invalid file type, must be audio/wav");
+	// 	return;
+	// }
+	if (file.size > 5e6) {
+		alert("File size should be less than 5MB (soft limit, can remove if needed)");
+		return;
+	}
+
+	const reader = new FileReader();
+	reader.onload = async () => {
+		try {
+			const data = reader.result;
+			if (typeof data === "string") throw new TypeError("unreachable");
+
+			const audio_ctx = new AudioContext({ sampleRate: SAMPLE_RATE, latencyHint: "playback" });
+			const decoded_pcm = await audio_ctx.decodeAudioData(data);
+			console.log(decoded_pcm);
+
+			if (decoded_pcm.numberOfChannels !== 1) {
+				alert("Audio file is not mono");
+				return;
+			}
+
+			const pcm = decoded_pcm.getChannelData(0); // device expects f32le
+
+			draw_waveform(pcm);
+
+			swslog(`Sending ${pcm.length} samples (${decoded_pcm.duration}s) to devices...`);
+			ws.send(pcm.buffer);
+		} catch (err) {
+			console.error(err);
+			alert("Error reading file, see browser console for details");
+		}
+	};
+	reader.readAsArrayBuffer(file);
+}
 
 
 
