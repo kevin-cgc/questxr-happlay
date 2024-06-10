865dc603173950a29c3fe04e570e19493410f0dc
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2024-06-10 01:04:53 -0700

    fix ParticipantID vis

--- server/frontend/js/participantid_analytics.mjs
+++ server/frontend/js/participantid_analytics.mjs
@@ -20,9 +20,9 @@ const APC = {
 
 class ParticipantID {
 	/**
-	 * @type {string | null}
+	 * @type {{ participant_id: string, participant_id_digest: string } | null}
 	 */
-	#participant_id_digest = null;
+	#participant_id_info = null;
 
 	#filemeta_idbkv;
 
@@ -33,9 +33,13 @@ class ParticipantID {
 	constructor(filemeta_idbkv, last_participant_id_info) {
 		this.#filemeta_idbkv = filemeta_idbkv;
 
+		this.#participant_id_info = last_participant_id_info;
 		participantid_input.value = last_participant_id_info?.participant_id ?? "";
 
 		participantid_input.addEventListener("input", ev => {
+			if (this.#participant_id_info && participantid_input.value == this.#participant_id_info.participant_id) return participantinfo_div.classList.add("committed");
+			else if (!this.#participant_id_info  && participantid_input.value == "") return participantinfo_div.classList.add("disabled");
+
 			participantinfo_div.classList.remove("committed");
 			participantinfo_div.classList.remove("disabled");
 		});
@@ -98,8 +102,8 @@ class ParticipantID {
 	async #set(participant_id_info) {
 		participantid_input.value = participant_id_info?.participant_id ?? "";
 		await idbkv.set("last_participant_id_info", participant_id_info);
+		this.#participant_id_info = participant_id_info;
 		const participant_id_digest = participant_id_info?.participant_id_digest ?? null;
-		this.#participant_id_digest = participant_id_digest;
 		this.#filemeta_idbkv = participant_id_digest == null ?
 			DEFAULT_FILE_META_STORE :
 			await idbkv.createStore("file_metadata_"+participant_id_digest, "keyval");
@@ -118,11 +122,11 @@ class ParticipantID {
 	 * @returns {Promise<boolean>} false if sync not available
 	 */
 	async sync_file(entry, meta) {
-		if (!this.#participant_id_digest) return false;
+		if (!this.#participant_id_info) return false;
 
 
 		const u = new URL("/api/participant/file", location.origin);
-		u.searchParams.set("participant_id_digest", this.#participant_id_digest);
+		u.searchParams.set("participant_id_digest", this.#participant_id_info.participant_id_digest);
 
 		for (const key of Object.keys(meta)) {
 			u.searchParams.set(key, meta[key]);
@@ -146,10 +150,10 @@ class ParticipantID {
 	 * @returns {Promise<boolean>} false if sync not available
 	 */
 	async sync_file_meta(meta) {
-		if (!this.#participant_id_digest) return false;
+		if (!this.#participant_id_info) return false;
 
 		const u = new URL("/api/participant/file/meta", location.origin);
-		u.searchParams.set("participant_id_digest", this.#participant_id_digest);
+		u.searchParams.set("participant_id_digest", this.#participant_id_info.participant_id_digest);
 
 		for (const key of Object.keys(meta)) {
 			u.searchParams.set(key, meta[key]);
@@ -171,10 +175,10 @@ class ParticipantID {
 	 * @returns {Promise<string[] | false>}
 	 */
 	async get_synced_files() {
-		if (!this.#participant_id_digest) return false;
+		if (!this.#participant_id_info) return false;
 
 		const u = new URL("/api/participant/files", location.origin);
-		u.searchParams.set("participant_id_digest", this.#participant_id_digest);
+		u.searchParams.set("participant_id_digest", this.#participant_id_info.participant_id_digest);
 
 		const f = await fetch(u);
 		if (!f.ok) {
@@ -186,7 +190,7 @@ class ParticipantID {
 	}
 
 }
-const last_participant_id_info = await idbkv.get("last_participant_id_digest");
+const last_participant_id_info = await idbkv.get("last_participant_id_info");
 const DEFAULT_FILE_META_STORE = await idbkv.createStore("file_metadata_default", "keyval");
 export const PARTICIPANT_ID_GLO = new ParticipantID(DEFAULT_FILE_META_STORE, last_participant_id_info);
 
