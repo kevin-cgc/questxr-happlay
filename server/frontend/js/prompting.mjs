97f49bf31a71116194a81a276738d5b2fa61126d
Kevin John <58094058+kevin-cgc@users.noreply.github.com>
2024-06-13 13:21:12 -0700

    possible support for autosaving out of hf space

--- server/frontend/js/prompting.mjs
+++ server/frontend/js/prompting.mjs
@@ -1,5 +1,6 @@
 import { USE_API_PROMPT_UI } from "./appmode.mjs";
-import { notnull } from "./util.mjs";
+import { save_signal_blob_to_file } from "./folderfilepicker.mjs";
+import { notnull, sanitize_filename } from "./util.mjs";
 
 const genmodelpromptcont_div = /** @type {HTMLDivElement} */ (notnull(document.querySelector(".genmodelpromptcont")));
 const duration_range_input = /** @type {HTMLInputElement} */ (notnull(genmodelpromptcont_div.querySelector("input[type=range]")));
@@ -30,5 +31,50 @@ if (USE_API_PROMPT_UI) {
 		// cont_div.attributeStyleMap.set("min-height", CSS.percent(100));
 		cont_div.style.margin = "0px";
 		cont_div.style.minHeight = "100%";
+
+		try {
+			const submit_button = /** @type {HTMLButtonElement} */ (notnull(document.querySelector("gradio-app ##component-6")));
+			submit_button.addEventListener("click", async () => {
+				console.log("clicked submit");
+				const prompt = notnull(document.querySelector("gradio-app #component-3 textarea")).value;
+				const model = "audiogen"; //unknown, not in the UI
+				const og_signal_url = document.querySelector("gradio-app #component-8 a")?.href;
+
+				while (document.querySelector("gradio-app #component-8 a")?.href === og_signal_url) {
+					await new Promise(resolve => setTimeout(resolve, 100));
+				}
+				const signal_url = document.querySelector("gradio-app #component-8 a")?.href;
+
+				await save_gradio_signal_to_file({signal_url, prompt, model});
+			});
+		} catch (e) {
+			console.error("Error setting up submit button", e);
+		}
 	});
+}
+
+
+/**
+ * @param {{ signal_url: string, prompt: string, model: string }} param1
+ */
+async function save_gradio_signal_to_file({ signal_url, prompt, model }) {
+
+	const signal = await fetch(signal_url).then(resp => resp.blob());
+
+	const randid = Math.random().toString(36).substring(7);
+
+	const filename = sanitize_filename(`${model}_${prompt.slice(0, 50)}_${randid}.wav`);
+
+	await save_signal_blob_to_file(signal, {
+		name: filename,
+		filename,
+		prompt,
+		model,
+		origin: notnull(document.querySelector("gradio-app"))["src"],
+		sha265: "",
+		starred: false,
+		trash: false,
+		vote: 0,
+	});
+
 }
--- server/frontend/js/util.mjs
+++ server/frontend/js/util.mjs
@@ -8,3 +8,12 @@ export function notnull(v) {
 	if (v != null) return v;
 	else throw new TypeError("Unexpected null");
 }
+
+/**
+ *
+ * @param {string} filename
+ * @returns {string} sanitized filename
+ */
+export function sanitize_filename(filename) {
+	return filename.replace(/[^a-zA-Z0-9._-]/g, "_");
+}
